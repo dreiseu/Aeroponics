4565b859562ba4b0cf7e5d59f04db709209dab12
dreiseu <121779773+dreiseu@users.noreply.github.com>
2023-05-21 09:45:04 +0800

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -1,5 +1,10 @@
 #include <Arduino.h>
 
+// Ultrasonic
+// DS18
+// GSM
+// Peristaltic
+
 // Arduino Serial Monitor
 #define SerialMon Serial
 
@@ -17,102 +22,62 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
 
+// DS18B20 Library
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// DS18B20 Variables
+#define DS18PIN 3
+OneWire oneWire(DS18PIN);
+DallasTemperature sensors(&oneWire);
+
 // Relay Pins
-#define RELAY_PUMP1_PIN 10
-#define RELAY_PUMP2_PIN 11
-#define RELAY_PUMP3_PIN 12
-#define RELAY_PUMP4_PIN 13
+#define RELAY_PUMP_PIN 10
+#define RELAY_FAN_PIN 11
 
 // Pin State
 int pinState1 = LOW;
 int pinState2 = LOW;
-int pinState3 = LOW;
-int pinState4 = LOW;
 
 // Millis Function
 unsigned long previousMillis1 = 0;
 unsigned long previousMillis2 = 0;
-unsigned long previousMillis3 = 0;
-unsigned long previousMillis4 = 0;
 
 // Intervals
-unsigned long OnTime1 = 180000;
-unsigned long OffTime1 = 720000;
-unsigned long OnTime2 = 5000;
-unsigned long OffTime2 = 10000;
+unsigned long OnTime = 180000;
+unsigned long OffTime = 720000;
 
 // Threshold Values
 const int HUMID_THRESHOLD_UPPER = 100;
 const int HUMID_THRESHOLD_LOWER = 70;
 
 void towers() {
-  tower1();
-  tower2();
-  tower3();
+  tower();
 }
 
-void tower1() {
+void tower() {
   unsigned long currentMillis1 = millis();
-  if ((pinState1 == HIGH) && (currentMillis1 - previousMillis1 >= OnTime1)) {
+  if ((pinState1 == HIGH) && (currentMillis1 - previousMillis1 >= OnTime)) {
     pinState1 = LOW;
     previousMillis1 = currentMillis1;
-    digitalWrite(RELAY_PUMP1_PIN, pinState1);
+    digitalWrite(RELAY_PUMP_PIN, pinState1);
   }
-  else if ((pinState1 == LOW) && (currentMillis1 - previousMillis1 >= OffTime1)) {
+  else if ((pinState1 == LOW) && (currentMillis1 - previousMillis1 >= OffTime)) {
     pinState1 = HIGH;
     previousMillis1 = currentMillis1;
-    digitalWrite(RELAY_PUMP1_PIN, pinState1);
-  }
-}
-
-void tower2() {
-  unsigned long currentMillis2 = millis();
-  if ((pinState2 == HIGH) && (currentMillis2 - previousMillis2 >= OnTime1)) {
-    pinState2 = LOW;
-    previousMillis2 = currentMillis2;
-    digitalWrite(RELAY_PUMP2_PIN, pinState2);
-  }
-  else if ((pinState2 == LOW) && (currentMillis2 - previousMillis2 >= OffTime1)) {
-    pinState2 = HIGH;
-    previousMillis2 = currentMillis2;
-    digitalWrite(RELAY_PUMP2_PIN, pinState2);
-  }
-}
-  
-void tower3() {
-  unsigned long currentMillis3 = millis();
-  if ((pinState3 == HIGH) && (currentMillis3 - previousMillis3 >= OnTime1)) {
-    pinState3 = LOW;
-    previousMillis3 = currentMillis3;
-    digitalWrite(RELAY_PUMP3_PIN, pinState3);
-  }
-  else if ((pinState3 == LOW) && (currentMillis3 - previousMillis3 >= OffTime1)) {
-    pinState3 = HIGH;
-    previousMillis3 = currentMillis3;
-    digitalWrite(RELAY_PUMP3_PIN, pinState3);
+    digitalWrite(RELAY_PUMP_PIN, pinState1);
   }
 }
 
 void dht22() {
-  unsigned long currentMillis4 = millis();
+  unsigned long currentMillis2 = millis();
   float h = dht.readHumidity();
   float t = dht.readTemperature();
-  if (h < HUMID_THRESHOLD_LOWER) {
-    if ((pinState4 == HIGH) && (currentMillis4 - previousMillis4 >= OnTime2)) {
-      pinState4 == LOW;
-      previousMillis4 = currentMillis4;
-      digitalWrite(RELAY_PUMP4_PIN, pinState4);    
-    }
-    else if ((pinState4 == LOW) && (currentMillis4 - previousMillis4 >= OffTime2)) {
-      pinState4 = HIGH;
-      previousMillis4 = currentMillis4;
-      digitalWrite(RELAY_PUMP4_PIN, pinState4);
-    }
-  }
-  else if (h >= HUMID_THRESHOLD_LOWER) {
-    //SerialMon.println("The humidity is at 100%.");
-    digitalWrite(RELAY_PUMP4_PIN, LOW);
-  }
+  if
+}
+
+void ds18b20() {
+  sensors.requestTemperatures();
 }
 
 void displayLCD() {
@@ -125,6 +90,13 @@ void displayLCD() {
   lcd.print(F("E.Temp: "));
   lcd.print(dht.readTemperature());
   lcd.print(" C");
+  delay(2000);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(F("S.Temp: ");
+  lcd.print(sensors.getTempCByIndex(0));
+  lcd.print(" C");
+  delay(2000);
 }
 
 void setup() {
@@ -155,5 +127,6 @@ void setup() {
 void loop() {
   towers();
   dht22();
+  ds18b20();
   displayLCD();
 }
